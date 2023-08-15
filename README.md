2f14f939977eb8bd60b46594b6ad29587fd5e4bf
Kevin Suryajaya Soemarto <k.soemarto@homecredit.co.id>
2023-08-15 12:02:08 +0700

    update radio button

--- README.md
+++ README.md
@@ -4,15 +4,11 @@ It's a journey to develop our component Design System for Sales Force Journey
 Develop by SFJ
 
 ## Specification
-Node version 16.20.0
+Node version 14.18.1
 
-NPM version 8.19.4
+NPM version 6.14.15
 
 React version 17.0.2
 
-Storybook version 7.0.22
-
-## Getting Started
-npm run start
-
--> to run storybook for components documentation.
+## New Update 0.0.8
+Update Radio Button
--- src/Components/Radio.js
+++ src/Components/Radio.js
@@ -1,166 +1,113 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
 
 export const Radio = ({
-  options,
   name,
   label,
   value,
-  radioType,
   selected,
   setSelected,
-  tabIndex,
-  cy
+  cy,
+  id,
+  disabled,
 }) => {
-  const list = radioType === "list";
-  const [option, setOption] = useState([]);
   const [radioSelected, setRadioSelected] = useState(selected);
 
-  const styles = {
-    radioContainer: {
-      display: "flex",
-      flexDirection: list ? "column" : "row",
-      margin: "10px"
-    },
-    option: {
-      width: "100%",
-      margin: list ? "10px" : "0 10px",
-      border: "1px solid #B3B3B3",
-      padding: "2px",
-      display: "flex",
-      alignItems: "center",
-      backgroundColor: "unset",
-      color: "#9f9f9f",
-      cursor: "pointer",
-      borderRadius: "3px"
-    },
-    optionChecked: {
-      width: "100%",
-      margin: list ? "10px" : "0 10px",
-      border: "1px solid #E11931",
-      padding: "2px",
-      display: "flex",
-      alignItems: "center",
-      backgroundColor: "rgba(225,25,49,0.1)",
-      color: "#e11931",
-      cursor: "pointer",
-      borderRadius: "3px"
-    },
-    input: {
-      position: "absolute",
-      opacity: "0",
-      cursor: "pointer",
-      width: "auto",
-      height: "5%"
-    },
-    customRadio: {
-      borderRadius: "100%",
-      height: "13px",
-      width: "13px",
-      margin: "5px",
-      position: "relative"
-    },
-    label: {
-      margin: "-2px 5px 0px 5px",
-      fontSize: "0.8rem"
-    }
-  };
-
-  const radioChange = event => {
+  const radioChange = (event) => {
     setSelected(event.currentTarget.value);
   };
-
-  const optionsValidate = option.length < 5 && option.length > 1;
-
-  useEffect(() => {
-    setOption(options);
-  }, [options]);
 
   useEffect(() => {
     setRadioSelected(selected);
   }, [selected]);
 
   return (
-    <div style={styles.radioContainer}>
-      {option
-        ? optionsValidate
-          ? option.map((option, index) => (
-              <label
-                style={
-                  radioSelected === option[value]
-                    ? styles.optionChecked
-                    : styles.option
-                }
-                key={index}
-              >
-                <div
-                  style={styles.customRadio}
-                  className={
-                    radioSelected === option[value] ? "check" : "uncheck"
-                  }
-                ></div>
-                <input
-                  style={styles.input}
-                  type="radio"
-                  value={option[value]}
-                  checked={radioSelected === option[value]}
-                  onChange={event => radioChange(event)}
-                  name={name}
-                  tabIndex={
-                    tabIndex ? (index === 0 ? tabIndex : undefined) : undefined
-                  }
-                  data-cy={cy + `_` + index}
-                ></input>
-                <span style={styles.label}>{option[label]}</span>
-              </label>
-            ))
-          : ""
-        : console.log("nada")}
+    <div className="input-radio-item-wrapper">
+      <label
+        className={
+          "input-radio" +
+          (radioSelected === value ? " input-radio--selected" : "")
+        }
+      >
+        <span className="input-radio-checkmark"></span>
+        {label}
+        <input
+          id={id}
+          type="radio"
+          name={name}
+          value={value}
+          checked={radioSelected === value}
+          onChange={(event) => radioChange(event)}
+          data-cy={cy + `_` + value}
+          disabled={disabled}
+        />
+      </label>
       <style jsx="true">{`
-        .uncheck::before {
-          content: "";
+        .input-radio-item-wrapper {
+          display: flex;
+          flex-wrap: wrap;
+          margin: 10px;
+        }
+        .input-radio {
+          width: 100%;
+          border: 1px solid ${disabled ? "#A9A9A9" : "#404040"};
+          border-radius: 4px;
+          padding: 10px;
+          padding-left: 36px;
+          position: relative;
+          cursor: pointer;
+          color: ${disabled ? "#A9A9A9" : "#404040"};
+          background-color: ${disabled ? "#F4F4F4" : "#FFFFFF"};
+        }
+
+        .input-radio--selected {
+          background-color: ${disabled ? "#F4F4F4" : "#ecf6ee"};
+          color: ${disabled ? "#A9A9A9" : "#3eac58"};
+          border-color: ${disabled ? "#A9A9A9" : "#3eac58"};
+        }
+        .input-radio-checkmark {
           position: absolute;
-          width: 14px;
-          height: 14px;
+          top: 12px;
+          left: 10px;
+          height: 15px;
+          width: 15px;
           border: 1px solid #7b7b7b;
-          border-radius: 100%;
-          background: transparent;
-          margin: -2.5px -2px;
+          border-radius: 50%;
         }
-        .check::before {
+        .input-radio-checkmark::after {
           content: "";
           position: absolute;
-          width: 14px;
-          height: 14px;
-          border: 1px solid #e11931;
-          border-radius: 100%;
-          background: #fce8ea;
-          margin: -2.5px;
+          display: none;
         }
-        .check::after {
-          content: "";
-          width: 8px;
-          height: 8px;
-          background: #e11931;
+        /* hide input radio */
+        .input-radio input {
           position: absolute;
-          border-radius: 100%;
-          -webkit-transition: all 0.2s ease;
-          transition: all 0.2s ease;
-          margin: 1.5px;
+          opacity: 0;
+          cursor: pointer;
+        }
+        .input-radio--selected .input-radio-checkmark::after {
+          display: block;
+        }
+        .input-radio--selected .input-radio-checkmark {
+          display: block;
+          border-color: ${disabled ? "#A9A9A9" : "#3eac58"};
+        }
+        .input-radio .input-radio-checkmark::after {
+          top: 3px;
+          left: 3px;
+          width: 9px;
+          height: 9px;
+          border-radius: 50%;
+          background: ${disabled ? "#A9A9A9" : "#3eac58"};
+        }
+        @media only screen and (max-width: 450px) {
+          .input-radio {
+            font-size: 12px;
+          }
+          .input-radio-checkmark {
+            top: 9px;
+          }
         }
       `}</style>
     </div>
   );
-};
-
-Radio.propTypes = {
-  value: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  radioType: PropTypes.string.isRequired
-};
-
-Radio.defaultProps = {
-  value: "value",
-  label: "label",
-  radioType: "row"
 };
